8f34d4fd88174b1807dfd5be37ea1a7acc38077f
Abdul Azis <62172154+aziezgt12@users.noreply.github.com>
2023-08-22 23:05:19 +0700

    Update database.php

--- application/config/production/database.php
+++ application/config/production/database.php
@@ -63,7 +63,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'https://smartsertifikasi.co.id/',
+	'hostname' => 'localhost',
 	'username' => 'smaricoi_uKoneksi',
 	'password' => 'sm@rt2023',
 	'database' => 'smaricoi_etraining',
